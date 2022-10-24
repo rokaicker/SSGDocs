fe772ebe0923deb4cd7dd6d8e8fe989f4cc0af82
Rohan Kaicker <rkaicker@myseneca.ca>
2022-10-24 19:25:13 -0400

    restarting fresh

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/rkaicker/School/OSD600/SSGDocs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/rkaicker/School/OSD600/SSGDocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/rkaicker/School/OSD600/SSGDocs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/rkaicker/School/OSD600/SSGDocs/src/css/custom.css'),
-];
